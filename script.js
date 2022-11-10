a5b7da9e7a44afec4f8ee94695e88588c1781b74
rsheridan613 <rsheridan613@gmail.com>
2022-11-10 16:08:00 -0700

    added html and js

---
+++ script.js
@@ -0,0 +1,4 @@
+var userInput = document.getElementById("user-input");
+var userForm = document.getElementById("form-submit");
+var searchedCities = [];
+var APIkey = "appid=272572527f0f24c6a9098fc55c892b6c";
