4a89985dbe6a2c8c7fe3f069047b4f186f738901
rsheridan613 <rsheridan613@gmail.com>
2022-11-12 09:49:59 -0700

    added css file and elements in html

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ var userInput = document.getElementById("user-input");
 var userForm = document.getElementById("form-submit");
 var searchedCities = [];
 var APIkey = "appid=272572527f0f24c6a9098fc55c892b6c";
+
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
